fbb7f5c9795771f8de21751a231ad64b0c242c36
Ross Grambo <rgrambo@uw.edu>
2026-04-23 13:05:39 -0700

    Add GitHub Pages deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 export default defineConfig({
+  base: '/grambo-family-app/',
   plugins: [preact({ devToolsEnabled: false })],
   build: {
     outDir: 'dist',
